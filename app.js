f19f83b37b62af8ee7bc9d8e1dd3763fdc5b99a5
ClassicalJo <jb.barrientos@yahoo.com>
2021-09-15 00:45:39 -0300

    Setting up basic http server to start checking the endpoints

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-
+var port = process.env.PORT || 5500
 var logger = require('morgan');
 var auth0 = require('./authentication/auth0')
 var guard = require('express-jwt-permissions')()
@@ -11,12 +11,17 @@ var charactersRouter = require('./routes/characters')
 var moviesRouter = require('./routes/movies')
 
 var sequelize = require('./sequelize/index');
+const authenticate = require('./authentication/middleware');
 
 
 sequelize.authenticate()
     .then(() => console.log('success'))
     .catch(err => console.error('fail', err))
 var app = express();
+var http = require('http')
+app.set('port', port)
+
+var server = http.createServer(app)
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,10 +29,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(auth0)
+// app.use(auth0)
+// app.use(authenticate)
 
 app.use('/characters', charactersRouter)
 app.use('/movies', moviesRouter)
 app.use('/auth', authRouter)
 
-module.exports = app;
+app.get('/', (req, res) => {
+    console.log('getting / ')
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+})
+
+server.listen(port)
+
+module.exports = app
