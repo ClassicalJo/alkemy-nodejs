8e50a1bdb5cbf696c4eeca90d6a2bc5ed4f5ce14
ClassicalJo <jb.barrientos@yahoo.com>
2021-09-15 22:22:22 -0300

    Revert "Adding more Auth0 boilerplate, in particular: -Config needed for passport session, config needed for Auth0 passport session, and it's middleware"
    This reverts commit 5fbcb8ad1326aae96da5b1238e4694644d7a2812.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -8,36 +9,13 @@ var moviesRouter = require('./routes/movies')
 var indexRouter = require('./routes/index')
 
 var sequelize = require('./sequelize/index');
+const authenticate = require('./authentication/middleware');
+
 sequelize.authenticate()
     .then(() => console.log('success'))
     .catch(err => console.error('fail', err))
 
 var app = express();
-var session = require('express-session')
-var sessionConfig = {
-    secret: process.env.SESSION_SECRET,
-    cookie: {
-        secure: true
-    },
-    resave: false,
-    saveUninitialized: true,
-}
-var passport = require('passport')
-var Auth0Strategy = require("passport-auth0")
-var strategy = new Auth0Strategy({
-    domain: process.env.AUTH0_ISSUER,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_SECRET,
-    callbackURL: 'http://localhost:5500/callback'
-},
-    function (accessToken, refreshToken, extraParams, profile, done) {
-        return done(null, profile);
-    })
-
-app.use(session(sessionConfig))
-passport.use(strategy);
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(logger('dev'));
 app.use(express.json());
