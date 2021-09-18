c7ba401308fccf2a853862b0f013f08aaa418e17
ClassicalJo <jb.barrientos@yahoo.com>
2021-09-18 05:16:33 -0300

    Small status number fix for not finding a resource.

--- authentication/api/routes/characters/delete.js
+++ authentication/api/routes/characters/delete.js
@@ -7,7 +7,7 @@ router.delete('/', async (req, res, next) => {
 
     Character.destroy({ where: { id: req.body.id } })
         .then(response => {
-            response == 0 && res.status(200).send("No entry with that ID was found.")
+            response == 0 && res.status(404).send("No entry with that ID was found.")
             response !== 0 && res.status(200).send("Entry succesfully deleted")
         })
         .catch(err => next(err))
