1f4ea7bfb4c07420bf7db664d98effe0461679c1
ClassicalJo <jb.barrientos@yahoo.com>
2021-09-18 04:58:46 -0300

    Character validations - Delete. If ID is not a number, will respond with code 400: bad request.

--- authentication/api/routes/characters/delete.js
+++ authentication/api/routes/characters/delete.js
@@ -3,8 +3,9 @@ var router = express.Router()
 var Character = require('../../sequelize/models/character')
 
 router.delete('/', async (req, res, next) => {
-    let { id } = req.body
-    Character.destroy({ where: { id } })
+    if (typeof req.body.id !== 'number') return res.status(400).send("ID field must be an integer.")
+
+    Character.destroy({ where: { id: req.body.id } })
         .then(response => {
             response == 0 && res.status(200).send("No entry with that ID was found.")
             response !== 0 && res.status(200).send("Entry succesfully deleted")
