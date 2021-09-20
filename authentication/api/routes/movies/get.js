9e6b9b4feda3fdafff98b02167b8bb1e8979234e
ClassicalJo <jb.barrientos@yahoo.com>
2021-09-20 15:40:31 -0300

    Updating API's GET movies, following the same pattern used with Characters.
    1) ID first.
    2) If queries, search by queries.
    3) Else, return movies list.

--- authentication/api/routes/movies/get.js
+++ authentication/api/routes/movies/get.js
@@ -1,11 +1,35 @@
 var express = require('express')
 var router = express.Router()
 var Movie = require('../../sequelize/models/movie')
+var { limitToFields, getAvailableFields, hasQuery } = require('../../../../helpers/index')
+var { Op } = require('sequelize')
+var fields = getAvailableFields(Movie)
 
 router.get('/', async (req, res, next) => {
-    Movie.findAll({ attributes: ['image', 'title', 'creationDate'] })
-        .then(response => res.json(response))
-        .catch(err => next(err))
+    let limitedQueries = limitToFields(req.query, fields)
+    if (req.query.id) {
+        let id = Number(req.query.id)
+        if (Number.isNaN(id)) return res.status(400).send("ID field must be an integer.")
+        Movie.findOne({ where: { id } })
+            .then(response => {
+                if (!response) res.status(404).send('ID not found.')
+                else res.json(response)
+            })
+            .catch(err => next(err))
+    }
+    else if (hasQuery(limitedQueries)) {
+        let { rating, relatedCharacters } = limitedQueries
+        if (rating && Number.isNaN(Number(rating))) return res.status(400).send("Rating field must be an integer")
+        if (relatedCharacters) limitedQueries.relatedCharacters = { [Op.contains]: [relatedCharacters.split('&&')].flat() }
+        Movie.findAll({ where: limitedQueries })
+            .then(response => res.json(response))
+            .catch(err => next(err))
+    }
+    else {
+        Movie.findAll({ attributes: ['image', 'title', 'creationDate'] })
+            .then(response => res.json(response))
+            .catch(err => next(err))
+    }
 });
 
 module.exports = router
