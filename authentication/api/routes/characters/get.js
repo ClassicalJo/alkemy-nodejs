f86ea58524f9ff8ab50c7dafaf639bed60fb091e
ClassicalJo <jb.barrientos@yahoo.com>
2021-09-20 19:24:45 -0300

    Updating GET movies, replacing availableFields with a better helper function specific to queries and replaced the number checks with new helper function isNaN

--- authentication/api/routes/characters/get.js
+++ authentication/api/routes/characters/get.js
@@ -1,17 +1,18 @@
 var express = require('express')
 var router = express.Router()
 var Character = require('../../sequelize/models/character')
-var { limitToFields, getAvailableFields, hasQuery } = require('../../../../helpers/index')
+var { limitToFields, hasQuery, getQueriableFieldsFromModel, isNaN } = require('../../../../helpers/index')
 var { Op } = require('sequelize')
 
-var fields = getAvailableFields(Character)
+var fields = getQueriableFieldsFromModel(Character)
+
 
 router.get('/', async (req, res, next) => {
     let limitedQueries = limitToFields(req.query, fields)
 
     if (req.query.id) {
-        let id = Number(req.query.id)
-        if (Number.isNaN(id)) return res.status(400).send("ID field must be an integer.")
+        let { id } = req.query
+        if (isNaN(id)) return res.status(400).send("ID field must be an integer.")
 
         Character.findOne({ where: { id } })
             .then(response => {
@@ -22,8 +23,8 @@ router.get('/', async (req, res, next) => {
     }
     else if (hasQuery(limitedQueries)) {
         let { age, weight, relatedMovies } = limitedQueries
-        if (age && Number.isNaN(Number(age))) return res.status(400).send("Age field must be an integer")
-        if (weight && Number.isNaN(Number(weight))) return res.status(400).send("Weight field must be an integer")
+        if (age && isNaN(age)) return res.status(400).send("Age field must be an integer")
+        if (weight && isNaN(weight)) return res.status(400).send("Weight field must be an integer")
         if (relatedMovies) limitedQueries.relatedMovies = { [Op.contains]: [relatedMovies.split('&&')].flat() }
 
         Character.findAll({ where: limitedQueries })
