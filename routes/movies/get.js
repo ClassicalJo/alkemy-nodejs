230f3ff7b3c22493299a9a26a2db7aee00759b09
ClassicalJo <jb.barrientos@yahoo.com>
2021-09-20 15:38:56 -0300

    Updating app's GET movies so it can process queries.

--- routes/movies/get.js
+++ routes/movies/get.js
@@ -3,11 +3,16 @@ var axios = require('axios')
 var router = express.Router()
 
 router.get('/', (req, res, next) => {
-    let APIEndpoint = process.env.API_URL + '/movies'
+    let APIEndpoint = new URL(process.env.API_URL + '/movies')
+
+    for (let query in req.query) {
+        APIEndpoint.searchParams.append(query, req.query[query])
+    }
+
     let options = { headers: { 'Authorization': req.headers.authorization } }
 
     axios
-        .get(APIEndpoint, options)
+        .get(APIEndpoint.toString(), options)
         .then(response => res.json(response.data))
         .catch(err => res.json({ error: 'error', message: err.message }))
 })
