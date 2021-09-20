d7156cf436e573732dbc38b78d53d13e38e84fbb
ClassicalJo <jb.barrientos@yahoo.com>
2021-09-20 14:54:22 -0300

    Updating character get endpoint and now can switch between three results.
    The request query will be filtered to fields subject to queries, established by var fields, then:
    
    Option a)
    If the request query has an ID field, it will ignore all other field and try to resolve the query using that field only.
    - If the ID is not a number will respond with bad request.
    - If the database doesn't find the ID, will respond with 404.
    - If the database finds the ID, will respond with the resource.
    
    Option b) If req.query has any other kind of queries, will use all of them to return a filtered database. In this case, an empty array wont constitute a 404.
    
    Option c) In any other case, the API will respond with the Character list.

--- authentication/api/routes/characters/get.js
+++ authentication/api/routes/characters/get.js
@@ -1,11 +1,40 @@
 var express = require('express')
 var router = express.Router()
 var Character = require('../../sequelize/models/character')
+var { limitToFields, getAvailableFields, hasQuery } = require('../../../../helpers/index')
+var { Op } = require('sequelize')
+
+var fields = getAvailableFields(Character)
 
 router.get('/', async (req, res, next) => {
-    Character.findAll({ attributes: ['image', 'name'] })
-        .then(response => res.json(response))
-        .catch(err => next(err))
+    let limitedQueries = limitToFields(req.query, fields)
+
+    if (req.query.id) {
+        let id = Number(req.query.id)
+        if (Number.isNaN(id)) return res.status(400).send("ID field must be an integer.")
+
+        Character.findOne({ where: { id } })
+            .then(response => {
+                if (!response) res.status(404).send('ID not found.')
+                else res.json(response)
+            })
+            .catch(err => next(err))
+    }
+    else if (hasQuery(limitedQueries)) {
+        let { age, weight, relatedMovies } = limitedQueries
+        if (age && Number.isNaN(Number(age))) return res.status(400).send("Age field must be an integer")
+        if (weight && Number.isNaN(Number(weight))) return res.status(400).send("Weight field must be an integer")
+        if (relatedMovies) limitedQueries.relatedMovies = { [Op.contains]: [relatedMovies.split('&&')].flat() }
+
+        Character.findAll({ where: limitedQueries })
+            .then(response => res.json(response))
+            .catch(err => next(err))
+    }
+    else {
+        Character.findAll({ attributes: ['image', 'name'] })
+            .then(response => res.json(response))
+            .catch(err => next(err))
+    }
 });
 
 module.exports = router
