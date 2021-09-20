3481e092018e68c416b35b4e21db1b00f1c3e732
ClassicalJo <jb.barrientos@yahoo.com>
2021-09-20 19:23:11 -0300

    Updating the PUT movies, replacing most of the logic with helper functions and the response check with truthy and falsy values.

--- authentication/api/routes/movies/put.js
+++ authentication/api/routes/movies/put.js
@@ -1,28 +1,24 @@
 var express = require('express')
 var router = express.Router()
 var Movie = require('../../sequelize/models/movie')
+var idCheck = require('../../middleware/idCheck')
+var { limitToFields, getFieldsFromModel } = require('../../../../helpers')
 
+var fields = getFieldsFromModel(Movie)
+
+router.use(idCheck)
 router.put('/', (req, res, next) => {
-    if (typeof req.body.id !== 'number') return res.status(400).send("ID field must be an integer.")
-
-    Movie.update(createEditValues(req.body), { where: { id: req.body.id } })
+    let fieldsToUpdate = limitToFields(req.body, fields)
+    let options = { where: { id: req.body.id } }
+    Movie.update(fieldsToUpdate, options)
         .then(response => {
-            response[0] == 0 && res.status(404).send("Movie ID not found")
-            response[0] !== 0 && res.status(200).send("Movie updated successfully")
+            !response[0] && res.status(404).send("Movie ID not found")
+            response[0] && res.status(200).send("Movie updated successfully")
         })
         .catch(err => {
             if (err.errors[0].type == 'Validation error') res.status(400).send(err.message)
             else next(err)
         })
 })
-
-module.exports = router
-
-function createEditValues(obj) {
-    let editValues = {}
-    let fields = ['title', 'image', 'creationDate', 'rating', 'relatedCharacters']
-    fields.forEach(key => { if (obj[key] !== undefined) editValues[key] = obj[key] })
-    return editValues
-}
 
 module.exports = router
