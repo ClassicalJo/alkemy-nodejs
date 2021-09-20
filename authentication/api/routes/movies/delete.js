60d47869bd2efa00a11b38fea9b7c4ce1134491a
ClassicalJo <jb.barrientos@yahoo.com>
2021-09-20 19:21:40 -0300

    Adding idCheck middleware and refactored the response with truthy and falsy values.

--- authentication/api/routes/movies/delete.js
+++ authentication/api/routes/movies/delete.js
@@ -1,14 +1,14 @@
-var express = require('express')
+var express = require('express');
+var idCheck = require('../../middleware/idCheck');
 var router = express.Router()
 var Movie = require('../../sequelize/models/movie')
 
+router.use(idCheck)
 router.delete('/', async (req, res, next) => {
-    if (typeof req.body.id !== 'number') return res.status(400).send("ID field must be an integer.")
-
     Movie.destroy({ where: { id: req.body.id } })
         .then(response => {
-            response == 0 && res.status(404).send("No entry with that ID was found.")
-            response !== 0 && res.status(200).send("Entry succesfully deleted")
+            !response && res.status(404).send("No entry with that ID was found.")
+            response && res.status(200).send("Entry succesfully deleted")
         })
         .catch(err => next(err))
 });
